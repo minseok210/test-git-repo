022d6fe5e256a0062f0ed3a07f6359e0e72e19e6
minseok210 <78528121+minseok210@users.noreply.github.com>
2022-05-12 20:17:01 +0900

    Update btn_polling.py

--- btn_polling.py
+++ btn_polling.py
@@ -10,6 +10,6 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while 1:
-  if GPIO.input(button_pin) = GPIO.HIGH:
+  if GPIO.input(button_pin)=GPIO.HIGH:
     print("Button pushed")
   time.sleep(0.1)
